df795e35fa6c3c23ce368511da0082d3d94d8108
Chad Darby <darby@luv2code.com>
2019-12-17 06:59:35 -0500

    “updates”

---
+++ source-code/ecommerce-project-release-1.0/02-html-table/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
---
+++ source-code/ecommerce-project-release-1.0/02-html-table/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
