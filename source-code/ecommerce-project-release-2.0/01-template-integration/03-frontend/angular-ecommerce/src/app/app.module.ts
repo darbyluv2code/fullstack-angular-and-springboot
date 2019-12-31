4bb2b94de78cb5324a3bcd33556294cdc53be531
Chad Darby <darby@luv2code.com>
2019-12-30 20:28:06 -0500

    03-frontend

---
+++ source-code/ecommerce-project-release-2.0/01-template-integration/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
