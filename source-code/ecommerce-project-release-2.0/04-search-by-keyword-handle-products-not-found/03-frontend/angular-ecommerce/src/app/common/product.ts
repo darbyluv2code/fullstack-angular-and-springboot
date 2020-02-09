f06d6d197636a907490415661332702b97c88e73
Chad Darby <darby@luv2code.com>
2020-02-09 08:56:48 -0500

    search by keyword - handle for products not found

---
+++ source-code/ecommerce-project-release-2.0/04-search-by-keyword-handle-products-not-found/03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdate: Date;
+}
