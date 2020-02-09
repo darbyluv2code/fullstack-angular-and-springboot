68d0cf16b000a6e7086bc5c18df0a95fb89599da
Chad Darby <darby@luv2code.com>
2020-02-09 14:25:17 -0500

    update

---
+++ source-code/ecommerce-project-release-1.0/03-product-images/ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
