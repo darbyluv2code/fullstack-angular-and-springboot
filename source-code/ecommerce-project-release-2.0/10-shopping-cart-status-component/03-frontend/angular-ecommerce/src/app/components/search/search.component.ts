e26c6149aba6055e47364a0b294a5294e95e2af5
Chad Darby <darby@luv2code.com>
2020-03-28 16:35:43 -0400

    10-shopping-cart-status-component

---
+++ source-code/ecommerce-project-release-2.0/10-shopping-cart-status-component/03-frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  doSearch(value: string) {
+    console.log(`value=${value}`);
+    this.router.navigateByUrl(`/search/${value}`);
+  }
+}
