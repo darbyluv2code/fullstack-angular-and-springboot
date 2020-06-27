bec4d7598dfc5d7eaaa5e9db3207088def8454e3
Chad Darby <darby@luv2code.com>
2020-06-27 17:47:49 -0400

    checkout-form-populate-country-and-state-dropdown-list

---
+++ source-code/ecommerce-project-release-2.0/20-checkout-form-populate-country-and-state-dropdown-list/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/State.java
@@ -0,0 +1,24 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="state")
+@Data
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="country_id")
+    private Country country;
+
+}
