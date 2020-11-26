07257820fbdce5c71b2d9edb1e0a61998fc404ea
Chad Darby <darby@luv2code.com>
2020-11-25 19:37:41 -0500

    25-checkout-form-save-the-order-backend

---
+++ source-code/ecommerce-project-release-2.0/25-checkout-form-save-the-order-backend/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Country.java
@@ -0,0 +1,41 @@
+package com.luv2code.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="code")
+    private String code;
+
+    @Column(name="name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ source-code/ecommerce-project-release-2.0/25-checkout-form-save-the-order-backend/03-frontend/angular-ecommerce/src/app/common/product-category.ts
@@ -0,0 +1,4 @@
+export class ProductCategory {
+    id: number;
+    categoryName: string;
+}
