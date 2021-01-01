f5118df45dd24f043cfb16131753a0fad399234d
Chad Darby <darby@luv2code.com>
2020-12-31 21:01:46 -0500

    29-security-vip-access-route-guard

---
+++ source-code/ecommerce-project-release-2.0/29-security-vip-access-route-guard/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa37fi2hxbFprF9s5d6',
+        issuer: 'https://dev-5389590.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
