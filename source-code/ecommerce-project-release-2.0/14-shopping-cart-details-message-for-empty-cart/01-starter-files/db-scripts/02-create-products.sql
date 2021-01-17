12f1ed0fda0613a11f4f345537529907a99ad17a
Chad Darby <darby@luv2code.com>
2021-01-16 23:17:28 -0500

    fix case of db tables

--- source-code/ecommerce-project-release-2.0/14-shopping-cart-details-message-for-empty-cart/01-starter-files/db-scripts/02-create-products.sql
+++ source-code/ecommerce-project-release-2.0/14-shopping-cart-details-message-for-empty-cart/01-starter-files/db-scripts/02-create-products.sql
@@ -43,34 +43,34 @@ AUTO_INCREMENT = 1;
 -- Add sample data
 -- -----------------------------------------------------
 
-INSERT INTO PRODUCT_CATEGORY(CATEGORY_NAME) VALUES ('BOOKS');
+INSERT INTO product_category(category_name) VALUES ('BOOKS');
 
-INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
-UNIT_PRICE, CATEGORY_ID,DATE_CREATED)
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
 VALUES ('BOOK-TECH-1000', 'JavaScript - The Fun Parts', 'Learn JavaScript',
 'assets/images/products/placeholder.png'
 ,1,100,19.99,1, NOW());
 
-INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
-UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
 VALUES ('BOOK-TECH-1001', 'Spring Framework Tutorial', 'Learn Spring',
 'assets/images/products/placeholder.png'
 ,1,100,29.99,1, NOW());
 
-INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
-UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
 VALUES ('BOOK-TECH-1002', 'Kubernetes - Deploying Containers', 'Learn Kubernetes',
 'assets/images/products/placeholder.png'
 ,1,100,24.99,1, NOW());
 
-INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
-UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
 VALUES ('BOOK-TECH-1003', 'Internet of Things (IoT) - Getting Started', 'Learn IoT',
 'assets/images/products/placeholder.png'
 ,1,100,29.99,1, NOW());
 
-INSERT INTO PRODUCT (SKU, NAME, DESCRIPTION, IMAGE_URL, ACTIVE, UNITS_IN_STOCK,
-UNIT_PRICE, CATEGORY_ID, DATE_CREATED)
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,
+unit_price, category_id, date_created)
 VALUES ('BOOK-TECH-1004', 'The Go Programming Language: A to Z', 'Learn Go',
 'assets/images/products/placeholder.png'
 ,1,100,24.99,1, NOW());
