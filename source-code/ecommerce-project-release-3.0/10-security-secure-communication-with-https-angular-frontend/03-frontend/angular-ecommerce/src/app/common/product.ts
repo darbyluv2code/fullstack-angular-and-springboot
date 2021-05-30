9a3419248e168c93cacd2678d1a0b9ffd1f981d5
Chad Darby <darby@luv2code.com>
2021-05-30 13:43:42 -0400

    10-security-secure-communication-with-https-angular-frontend

---
+++ source-code/ecommerce-project-release-3.0/10-security-secure-communication-with-https-angular-frontend/03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    id: string;
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdate: Date;
+}
