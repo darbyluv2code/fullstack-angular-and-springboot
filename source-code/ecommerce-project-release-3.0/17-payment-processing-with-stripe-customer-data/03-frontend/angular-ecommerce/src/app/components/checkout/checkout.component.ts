162346af206579e2729939a57999caf2ba4fad77
Chad Darby <darby@luv2code.com>
2021-11-21 13:19:59 -0500

    16-payment-processing-with-stripe-bug-fix-reload-page-reset-cart

--- source-code/ecommerce-project-release-3.0/17-payment-processing-with-stripe-customer-data/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ source-code/ecommerce-project-release-3.0/17-payment-processing-with-stripe-customer-data/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -338,6 +338,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
     
     // reset the form
     this.checkoutFormGroup.reset();
--- source-code/ecommerce-project-release-3.0/19-payment-processing-with-stripe-email-support/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ source-code/ecommerce-project-release-3.0/19-payment-processing-with-stripe-email-support/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -346,6 +346,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
     
     // reset the form
     this.checkoutFormGroup.reset();
