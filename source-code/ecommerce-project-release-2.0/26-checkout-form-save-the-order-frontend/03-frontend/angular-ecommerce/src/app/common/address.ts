d56e81fc3f50c5d6606fd97f0fbc2020f00d64a2
Chad Darby <darby@luv2code.com>
2022-09-01 02:16:28 -0400

    updates for Angular 14 and Okta 5

--- source-code/ecommerce-project-release-2.0/26-checkout-form-save-the-order-frontend/03-frontend/angular-ecommerce/src/app/common/address.ts
+++ source-code/ecommerce-project-release-2.0/26-checkout-form-save-the-order-frontend/03-frontend/angular-ecommerce/src/app/common/address.ts
@@ -1,7 +1,9 @@
 export class Address {
-    street: string;
-    city: string;
-    state: string;
-    country: string;
-    zipCode: string;
+    
+    constructor(public street: string,
+                public city: string,
+                public state: string,
+                public country: string,
+                public zipCode: string) { }
+
 }
